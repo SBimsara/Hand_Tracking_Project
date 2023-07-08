51ab024f3135f11b9db3c18990abef6ffa3ef098
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-07-08 11:10:15 +0530

    added fps calculation

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -7,6 +7,10 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
 
@@ -18,6 +22,10 @@ while True:
        for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
     cv2.imshow("Image",img)
     cv2.waitKey(1)
