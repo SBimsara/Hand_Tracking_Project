cdac92f2b41b92b0dd3178810a54943fa4448773
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-07-09 10:22:07 +0530

    identified every landmark

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -20,7 +20,15 @@ while True:
 
     if results.multi_hand_landmarks:
        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+           for id, lm in enumerate(handLms.landmark):
+               print(id, lm)
+               h, w, c= img.shape
+               cx, cy = int(lm.x*w), int(lm.y*h)
+               print(id, cx, cy)
+
+           mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
