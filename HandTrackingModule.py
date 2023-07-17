86cf502e059399822e33beb212657d3f2218d9c8
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-07-17 13:44:31 +0530

    changed arguments' data type

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -7,8 +7,8 @@ class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
+        self.detectionCon = int(detectionCon)
+        self.trackCon = int(trackCon)
 
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
